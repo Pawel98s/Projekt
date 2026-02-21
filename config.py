550f6e8297ead3bf9f985ba3ad5ff68d746a99fc
Pawel <pawel98sibiga@gmail.com>
2026-02-21 14:08:47 +0100

    hidden password database

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "cookie")
 
-    DB_NAME = os.getenv("DB_NAME", "testowa")
-    DB_USER = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD = os.getenv("DB_PASSWORD", "lahata")
-    DB_HOST = os.getenv("DB_HOST", "localhost")
-    DB_PORT = os.getenv("DB_PORT", "5432")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_USER = os.getenv("DB_USER")
+    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PORT = os.getenv("DB_PORT")
 
     OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
     EMBEDDING_MODEL = os.getenv("EMBEDDING_MODEL", "all-MiniLM-L6-v2")
